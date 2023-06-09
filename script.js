12adedd4f737eba1dce9e77aca679fd572245e1b
Ibrahim <22484328+ibrahimmudassar@users.noreply.github.com>
2023-06-09 05:28:40 +0100

    first working prototype

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-var source = document.getElementById("entry-template").innerHTML;
-var template = Handlebars.compile(source);
+Handlebars.registerHelper("formatTime", function (isoString) {
+  const utcTime = moment.utc(isoString);
+  return utcTime.tz("America/New_York").format("LLLL");
+});
 
-let url =
-  "https://raw.githubusercontent.com/ibrahimmudassar/Rutgers-Foodies/main/events.json";
-fetch(url)
-  .then((res) => res.json())
-  .then((out) => console.log("Checkout this JSON! ", out))
-  .catch((err) => {
-    throw err;
+$(function () {
+  var url =
+    "https://raw.githubusercontent.com/ibrahimmudassar/Rutgers-Foodies/main/events.json";
+  $.getJSON(url, function (data) {
+    var template = Handlebars.compile($("#template").html());
+    $("#content").html(template({ people: data["data"] }));
   });
-console.log(context);
-var html = template(context);
-document.getElementById("out").innerHTML = html;
+});
