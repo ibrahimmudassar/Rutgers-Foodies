4684c835a158a854be85970f2e831edd5cf803b5
Ibrahim Mudassar <22484328+ibrahimmudassar@users.noreply.github.com>
2023-08-30 10:15:22 -0400

    adding colored map

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import json
+from dataclasses import dataclass
 from datetime import datetime
+from math import atan2, cos, radians, sin, sqrt
 
 import folium
 import pandas as pd
+import pytz
 import requests as re
 from bs4 import BeautifulSoup
+from folium import plugins
 
 # this takes the descriptions which have html tags and weird special characters and seeks to normalize them
 # this is not perfect but it's just the barebones
@@ -12,15 +16,57 @@ from bs4 import BeautifulSoup
 
 def description_deformat(input: str) -> str:
 
-    soup = BeautifulSoup(input, features="html.parser").text
+    soup = BeautifulSoup(input, features="html.parser").get_text()
     soup = soup.replace("\xa0", " ")
     soup = soup.replace("\n", " ")
 
     return soup
 
 
+def distance(lat1, lon1, lat2, lon2):
+    R = 6371  # Radius of the Earth in kilometers
+    lat1, lon1, lat2, lon2 = map(radians, [lat1, lon1, lat2, lon2])
+    dlat = lat2 - lat1
+    dlon = lon2 - lon1
+    a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+    distance_km = R * c
+    distance_m = distance_km * 1000
+    return distance_m
+
+
+@dataclass
+class Campus:
+    name: str
+    lat: float
+    lon: float
+    color: str
+
+
+def campus_to_icon(lat, lon):
+    campuses = [Campus("C", 40.4992699, -74.44746, 'blue'),  # College Ave
+                Campus("L", 40.5260255, -74.4377431, 'red'),  # Livingston
+                Campus("B", 40.5270353, -74.4563511, 'orange'),  # Busch
+                Campus("C/D", 40.4846662, -74.4366894,
+                       'green'),  # Cook / Douglass
+                ]
+
+    distance_to_each_campus = []
+    # find how far each campus is from the coordinate
+    for campus in campuses:
+        distance_to_each_campus.append(
+            (distance(lat, lon, campus.lat, campus.lon), campus))
+
+    nearest_campus = min(distance_to_each_campus, key=lambda x: x[0])
+    return nearest_campus[1]
+
+
+# Making events.json
 # get the date today, will be used for the event gathering
-today = datetime.now().isoformat()
+todayutc = datetime.utcnow()
+eastern_tz = pytz.timezone('US/Eastern')  # Rutgers timezone
+eastern_now = todayutc.replace(tzinfo=pytz.utc).astimezone(eastern_tz)
+today = eastern_now.isoformat()
 
 # find how many total events there are
 response = re.get(
@@ -33,21 +79,29 @@ response = re.get(
     f"https://rutgers.campuslabs.com/engage/api/discovery/event/search?endsAfter={today}&orderByField=endsOn&orderByDirection=ascending&status=Approved&take={count}"
 ).json()
 
-rows = []
-for event in response["value"]:
-    if "Free Food" in ", ".join(event["benefitNames"]):
-        rows.append(event)
+
+# Only events with Free Food as a benefit
+rows = [event for event in response['value']
+        if "Free Food" in event["benefitNames"]]
 
 df = pd.DataFrame(rows)
+# General column formatting
 df["description"] = df["description"].map(description_deformat)
 df["benefitNames"] = [", ".join(i) for i in df["benefitNames"]]
 default_background = "https://wallpapers.com/images/hd/rutgers-white-r-logo-uh1s17dgdpw9uhif.jpg"
 df["imagePath"] = [
     default_background if img is None else f"https://se-images.campuslabs.com/clink/images/{str(img)}" for img in df["imagePath"]]
+df["organizationProfilePicture"] = [
+    default_background if img is None else f"https://se-images.campuslabs.com/clink/images/{str(img)}" for img in df["organizationProfilePicture"]]
 df["startsOn"] = pd.to_datetime(df["startsOn"])
 df['latitude'] = pd.to_numeric(df['latitude'])
 df['longitude'] = pd.to_numeric(df['longitude'])
 
+with open("events.json", "w") as file:
+    unformatted = json.loads(df.to_json(orient="table"))
+    json.dump(unformatted, file, indent=4, sort_keys=True)
+
+# Making the map
 # makes filtered df with lat and lon available rows only
 filtered = df.query('latitude.notnull() and longitude.notnull()')
 # Create a centered map
@@ -56,12 +110,13 @@ map_sf = folium.Map(location=RU_COORDINATES, zoom_start=13)
 
 # Add markers to the map for each location in the dataframe
 for index, row in filtered.iterrows():
+    campus_icon = campus_to_icon(row['latitude'], row['longitude'])
     folium.Marker(location=[row['latitude'], row['longitude']],
-                  popup=row['name']).add_to(map_sf)
+                  popup=row['name'], icon=folium.Icon(color=campus_icon.color)).add_to(map_sf)
+
+# plugins
+plugins.Fullscreen().add_to(map_sf)
+plugins.LocateControl().add_to(map_sf)  # Geolocate
 
 # Save the map as an HTML file
 map_sf.save('events_map.html')
-
-with open("events.json", "w") as file:
-    unformatted = json.loads(df.to_json(orient="table"))
-    json.dump(unformatted, file, indent=4, sort_keys=True)
