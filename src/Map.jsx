b1e9c0c53d6fd287eec6dba4c5c0fc5d30ef0ac0
Ibrahim <22484328+ibrahimmudassar@users.noreply.github.com>
2024-04-04 20:45:16 -0400

    trying a proxy

--- src/Map.jsx
+++ src/Map.jsx
@@ -6,7 +6,7 @@ export default function Map() {
       <NavigationBar />
       <iframe
         className="size-full"
-        src="/Rutgers-Foodies/events_map.html"
+        src="https://rawcdn.githack.com/ibrahimmudassar/Rutgers-Foodies/5531ee88da737707a843a7b9dc7d6b1695600430/events_map.html"
       ></iframe>
     </div>
   );
